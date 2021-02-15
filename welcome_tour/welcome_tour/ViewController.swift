3d51b77821ad295b0a13e86e299768c38e4a398b
WelcomeFred <fred-sargsyan@mail.ru>
2021-02-15 20:32:47 +0400

    project start

---
+++ welcome_tour/welcome_tour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  welcome_tour
+//
+//  Created by User on 15.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
