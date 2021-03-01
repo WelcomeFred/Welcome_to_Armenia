1db990e5783330f5fc689e8d832a5f8b84ed205a
Norayr Grigoryan <grigoryannorayr@gmail.com>
2021-03-01 14:51:37 +0400

    Astghik: 1. Connect CocoaPods 2. Install pod - iCarousel

---
+++ welcome_tour/Pods/Target Support Files/Pods-welcome_tour/Pods-welcome_tour-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_welcome_tour : NSObject
+@end
+@implementation PodsDummy_Pods_welcome_tour
+@end
--- welcome_tour/welcome_tour/Pages/More/Help/HelpVC.swift
+++ welcome_tour/welcome_tour/Pages/More/Help/HelpVC.swift
@@ -6,28 +6,52 @@
 //
 
 import UIKit
+import iCarousel
 
-class HelpVC: UIViewController {
+class HelpVC: UIViewController, iCarouselDataSource {
 
     @IBOutlet weak var titleBlurEffect: UIVisualEffectView!
     @IBOutlet weak var titleName: UILabel!
+    let myCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .coverFlow /* cylndr , rotary , */
+        
+        return view
+           }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(myCarousel )
+        myCarousel.dataSource = self
+        
+        //myCarousel.autoscroll = -0.5
+        
+        myCarousel.frame = CGRect(x: 0,
+                                  y: 200,
+                                  width: view.frame.size.width,
+                                  height: 400)
     }
+    func numberOfItems(in carousel: iCarousel) -> Int {
+            return 4    }
+        
+        func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+            
+            let view = UIView(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: self.view.frame.size.width/1.4,
+                                            height: 450))
+            view.backgroundColor = .white
+            let imageview = UIImageView(frame:  view.bounds)
+            view.addSubview(imageview)
+            imageview.contentMode = .scaleToFill
+            imageview.image = UIImage(named: "iCarousel\(index+1)")
+           
+            
+            
+            return view
+        }
     
     @IBAction func back(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
